32a64e9ce244f8e32cde80968fcc4f6faed1b2de
Brian Davidson <bkdson@gmail.com>
2020-10-03 23:00:46 -0400

    resolve unary operator error

--- common.bash
+++ common.bash
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 function debug(){
-    if [ ${debug} == "true"  ]; then echo "DEBUG: ${1}" ; fi
+    if [[ ${debug} == "true"  ]]; then echo "DEBUG: ${1}" ; fi
 }
 
 function logerror(){
